5a53990a10db4e7b7c35f84520f4f86f02cff810
Tomas Kala <tomik.kala@gmail.com>
2019-07-30 21:02:41 +0200

    Continuous univariate distributions stub

---
+++ pynference/distributions/continuous_univariate.py
@@ -0,0 +1,101 @@
+from distribution import Distribution, ExponentialFamily
+
+
+class Beta(ExponentialFamily):
+    _constraints = {}
+    _support = None
+
+    @property
+    def mean(self):
+        pass
+
+    @property
+    def variance(self):
+        pass
+
+    def _log_prob(self, x):
+        pass
+
+    def _sample(self, sample_shape, random_state):
+        pass
+
+    @property
+    def natural_parameter(self):
+        pass
+
+    @property
+    def log_normalizer(self):
+        pass
+
+    def base_measure(self, x):
+        pass
+
+    def sufficient_statistic(self, x):
+        pass
+
+
+class Cauchy(Distribution):
+    _constraints = {}
+    _support = None
+
+    @property
+    def mean(self):
+        pass
+
+    @property
+    def variance(self):
+        pass
+
+    def _log_prob(self, x):
+        pass
+
+    def _sample(self, sample_shape, random_state):
+        pass
+
+
+class Exponential(ExponentialFamily):
+    pass
+
+
+class Gamma(ExponentialFamily):
+    pass
+
+
+class InvGamma(ExponentialFamily):
+    pass
+
+
+class Laplace(Distribution):
+    pass
+
+
+class Logistic(Distribution):
+    pass
+
+
+class LogNormal(ExponentialFamily):
+    pass
+
+
+class Normal(ExponentialFamily):
+    pass
+
+
+class Pareto(Distribution):
+    pass
+
+
+class T(Distribution):
+    pass
+
+
+class TruncatedExponential(Distribution):
+    pass
+
+
+class TruncatedNormal(Distribution):
+    pass
+
+
+class Uniform(Distribution):
+    pass
