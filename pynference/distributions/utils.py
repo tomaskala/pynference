10e202f1da61604102c18de9f0df242ffdf9ba0f
Tomas Kala <tomik.kala@gmail.com>
2019-08-01 21:45:07 +0200

    Broadcast shapes function

--- pynference/distributions/utils.py
+++ pynference/distributions/utils.py
@@ -1,4 +1,5 @@
 import numbers
+from typing import Tuple
 
 import numpy as np
 
@@ -12,3 +13,23 @@ def check_random_state(seed):
         return seed
     else:
         raise ValueError(f"Invalid random seed {seed}.")
+
+
+def broadcast_shapes(*shapes: Tuple[int]) -> Tuple[int]:
+    if len(shapes) == 1:
+        return shapes[0]
+
+    ndim = max(len(shape) for shape in shapes)
+    shapes = np.array([(1,) * (ndim - len(shape)) + shape for shape in shapes])
+
+    min_shape = np.min(shapes, axis=0)
+    max_shape = np.max(shapes, axis=0)
+
+    result_shape = np.where(min_shape == 0, 0, max_shape)
+
+    if not np.all((shapes == result_shape) | (shapes == 1)):
+        raise ValueError(
+            f"Incompatible shapes for broadcasting: {tuple(map(tuple, shapes))}."
+        )
+
+    return tuple(result_shape)  # type: ignore
---
+++ tests/distributions/test_utils.py
@@ -0,0 +1,76 @@
+import pytest
+
+from pynference.distributions.utils import broadcast_shapes
+
+
+def test_broadcast_single_arg():
+    x = (4, 3)
+
+    assert broadcast_shapes(x) == (4, 3)
+
+
+def test_broadcast_2d():
+    x = (4, 1)
+    y = (4, 3)
+
+    assert broadcast_shapes(x, y) == (4, 3)
+
+
+def test_broadcast_3d():
+    x = (4, 1, 1)
+    y = (4, 3, 1)
+    z = (4, 3, 2)
+
+    assert broadcast_shapes(x, y, z) == (4, 3, 2)
+
+
+def test_broadcast_5d():
+    x = (1, 4, 5, 1, 1)
+    y = (2, 4, 5, 6, 2)
+
+    assert broadcast_shapes(x, y) == (2, 4, 5, 6, 2)
+
+
+def test_broadcast_correct_order_matters():
+    x = (10, 1)
+    y = (10, 1)
+    z1 = (10, 2)
+    z2 = (2, 10)
+
+    assert broadcast_shapes(x, y, z1) == (10, 2)
+
+    with pytest.raises(ValueError):
+        broadcast_shapes(x, y, z2)
+
+
+def test_broadcast_incompatible_richer_shapes():
+    x = (2, 5)
+    y = (4, 5)
+
+    with pytest.raises(ValueError):
+        broadcast_shapes(x, y)
+
+
+def test_broadcast_require_explicit_ones():
+    x = (4,)
+    y = (4, 3)
+    z = (4, 3, 2)
+
+    with pytest.raises(ValueError):
+        broadcast_shapes(x, y, z)
+
+
+def test_broadcast_incompatible1():
+    x = (4, 5)
+    y = (2, 6)
+
+    with pytest.raises(ValueError):
+        broadcast_shapes(x, y)
+
+
+def test_broadcast_incompatible2():
+    x = (1, 2, 3)
+    y = (3, 2)
+
+    with pytest.raises(ValueError):
+        broadcast_shapes(x, y)
