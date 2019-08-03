fcc2aca63018438d2e92823bb95951d090d1dcef
Tomas Kala <tomik.kala@gmail.com>
2019-08-03 16:22:02 +0200

    Fix a typo

--- pynference/constants.py
+++ pynference/constants.py
@@ -2,6 +2,6 @@ from typing import Union
 
 import numpy as np
 
-ArrayLike = Union[float, np.ndarray]  # Return type of probability a evaluation.
+ArrayLike = Union[float, np.ndarray]  # Return type of a probability evaluation.
 Parameter = Union[float, np.ndarray]  # Parameters of probability distributions.
 Variate = Union[float, np.ndarray]  # Return type of distribution sampling.
