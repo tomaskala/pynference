607570f67284a0bd49448a6642720ee6819e9ee2
Tomas Kala <tomik.kala@gmail.com>
2019-09-29 14:26:17 +0200

    Discrete univariate exponential families

--- pynference/distributions/__init__.py
+++ pynference/distributions/__init__.py
@@ -1,1 +1,2 @@
 from pynference.distributions.continuous_univariate import *  # noqa
+from pynference.distributions.discrete_univariate import *  # noqa
--- pynference/distributions/discrete_univariate.py
+++ pynference/distributions/discrete_univariate.py
@@ -2,7 +2,7 @@ from typing import Dict, Tuple
 
 import numpy as np
 from numpy.random import RandomState
-from scipy.special import gammaln
+from scipy.special import binom, gamma, gammaln
 
 from pynference.constants import ArrayLike, Parameter, Shape, Variate
 from pynference.distributions.constraints import (
@@ -61,17 +61,17 @@ class Bernoulli(ExponentialFamily):
 
     @property
     def natural_parameter(self) -> Tuple[Parameter, ...]:
-        pass  # TODO
+        return (np.log(self.p) - np.log1p(-self.p),)
 
     @property
     def log_normalizer(self) -> Parameter:
-        pass  # TODO
+        return -np.log1p(-self.p)
 
     def base_measure(self, x: Variate) -> ArrayLike:
-        pass  # TODO
+        return 1.0
 
     def sufficient_statistic(self, x: Variate) -> Tuple[ArrayLike, ...]:
-        pass  # TODO
+        return (x,)
 
 
 class Binomial(ExponentialFamily):
@@ -122,17 +122,17 @@ class Binomial(ExponentialFamily):
 
     @property
     def natural_parameter(self) -> Tuple[Parameter, ...]:
-        pass  # TODO
+        return (np.log(self.p) - np.log1p(-self.p),)
 
     @property
     def log_normalizer(self) -> Parameter:
-        pass  # TODO
+        return -self.n * np.log1p(-self.p)
 
     def base_measure(self, x: Variate) -> ArrayLike:
-        pass  # TODO
+        return binom(self.n, x)
 
     def sufficient_statistic(self, x: Variate) -> Tuple[ArrayLike, ...]:
-        pass  # TODO
+        return (x,)
 
 
 class Dirac(Distribution):
@@ -176,161 +176,7 @@ class Dirac(Distribution):
         return np.broadcast_to(self.x, shape)
 
 
-class Geometric(ExponentialFamily):
-    _constraints: Dict[str, Constraint] = {"p": zero_one}
-    _support: Constraint = non_negative_integer
-
-    def __init__(
-        self, p: Parameter, check_parameters: bool = True, check_support: bool = True
-    ):
-        batch_shape = np.shape(p)
-        rv_shape = ()
-
-        self.p = p
-
-        super().__init__(
-            batch_shape=batch_shape,
-            rv_shape=rv_shape,
-            check_parameters=check_parameters,
-            check_support=check_support,
-        )
-
-    @property
-    def mean(self) -> Parameter:
-        return (1.0 - self.p) / self.p
-
-    @property
-    def variance(self) -> Parameter:
-        return (1.0 - self.p) / np.square(self.p)
-
-    def _log_prob(self, x: Variate) -> ArrayLike:
-        return x * np.log1p(-self.p) + np.log(self.p)
-
-    def _sample(self, sample_shape: Shape, random_state: RandomState) -> Variate:
-        return random_state.geometric(p=self.p, size=sample_shape + self.batch_shape)
-
-    @property
-    def natural_parameter(self) -> Tuple[Parameter, ...]:
-        pass  # TODO
-
-    @property
-    def log_normalizer(self) -> Parameter:
-        pass  # TODO
-
-    def base_measure(self, x: Variate) -> ArrayLike:
-        pass  # TODO
-
-    def sufficient_statistic(self, x: Variate) -> Tuple[ArrayLike, ...]:
-        pass  # TODO
-
-
-class NegativeBinomial(ExponentialFamily):
-    _constraints: Dict[str, Constraint] = {"r": positive, "p": zero_one}
-    _support: Constraint = non_negative_integer
-
-    def __init__(
-        self,
-        r: Parameter,
-        p: Parameter,
-        check_parameters: bool = True,
-        check_support: bool = True,
-    ):
-        batch_shape = broadcast_shapes(np.shape(r), np.shape(p))
-        rv_shape = ()
-
-        self.r, self.p = promote_shapes(r, p)
-
-        super().__init__(
-            batch_shape=batch_shape,
-            rv_shape=rv_shape,
-            check_parameters=check_parameters,
-            check_support=check_support,
-        )
-
-    @property
-    def mean(self) -> Parameter:
-        return self.r * self.p / (1.0 - self.p)
-
-    @property
-    def variance(self) -> Parameter:
-        return self.r * self.p / np.square(1.0 - self.p)
-
-    def _log_prob(self, x: Variate) -> ArrayLike:
-        return (
-            log_binomial_coefficient(self.r + x - 1, x)
-            + self.r * np.log1p(-self.p)
-            + x * np.log(self.p)
-        )
-
-    def _sample(self, sample_shape: Shape, random_state: RandomState) -> Variate:
-        return random_state.negative_binomial(
-            n=self.r, p=1.0 - self.p, size=sample_shape + self.batch_shape
-        )
-
-    @property
-    def natural_parameter(self) -> Tuple[Parameter, ...]:
-        pass  # TODO
-
-    @property
-    def log_normalizer(self) -> Parameter:
-        pass  # TODO
-
-    def base_measure(self, x: Variate) -> ArrayLike:
-        pass  # TODO
-
-    def sufficient_statistic(self, x: Variate) -> Tuple[ArrayLike, ...]:
-        pass  # TODO
-
-
-class Poisson(ExponentialFamily):
-    _constraints: Dict[str, Constraint] = {"rate": positive}
-    _support: Constraint = non_negative_integer
-
-    def __init__(
-        self, rate: Parameter, check_parameters: bool = True, check_support: bool = True
-    ):
-        batch_shape = np.shape(rate)
-        rv_shape = ()
-
-        self.rate = rate
-
-        super().__init__(
-            batch_shape=batch_shape,
-            rv_shape=rv_shape,
-            check_parameters=check_parameters,
-            check_support=check_support,
-        )
-
-    @property
-    def mean(self) -> Parameter:
-        return self.rate
-
-    @property
-    def variance(self) -> Parameter:
-        return self.rate
-
-    def _log_prob(self, x: Variate) -> ArrayLike:
-        return x * np.log(self.rate) - gammaln(x + 1) - self.rate
-
-    def _sample(self, sample_shape: Shape, random_state: RandomState) -> Variate:
-        return random_state.poisson(lam=self.rate, size=sample_shape + self.batch_shape)
-
-    @property
-    def natural_parameter(self) -> Tuple[Parameter, ...]:
-        pass  # TODO
-
-    @property
-    def log_normalizer(self) -> Parameter:
-        pass  # TODO
-
-    def base_measure(self, x: Variate) -> ArrayLike:
-        pass  # TODO
-
-    def sufficient_statistic(self, x: Variate) -> Tuple[ArrayLike, ...]:
-        pass  # TODO
-
-
-class Uniform(Distribution):
+class DiscreteUniform(Distribution):
     _constraints: Dict[str, Constraint] = {"lower": integer, "upper": integer}
 
     def __init__(
@@ -381,3 +227,157 @@ class Uniform(Distribution):
             size=sample_shape + self.batch_shape,
             dtype=self.lower.dtype,
         )
+
+
+class Geometric(ExponentialFamily):
+    _constraints: Dict[str, Constraint] = {"p": zero_one}
+    _support: Constraint = non_negative_integer
+
+    def __init__(
+        self, p: Parameter, check_parameters: bool = True, check_support: bool = True
+    ):
+        batch_shape = np.shape(p)
+        rv_shape = ()
+
+        self.p = p
+
+        super().__init__(
+            batch_shape=batch_shape,
+            rv_shape=rv_shape,
+            check_parameters=check_parameters,
+            check_support=check_support,
+        )
+
+    @property
+    def mean(self) -> Parameter:
+        return (1.0 - self.p) / self.p
+
+    @property
+    def variance(self) -> Parameter:
+        return (1.0 - self.p) / np.square(self.p)
+
+    def _log_prob(self, x: Variate) -> ArrayLike:
+        return x * np.log1p(-self.p) + np.log(self.p)
+
+    def _sample(self, sample_shape: Shape, random_state: RandomState) -> Variate:
+        return random_state.geometric(p=self.p, size=sample_shape + self.batch_shape)
+
+    @property
+    def natural_parameter(self) -> Tuple[Parameter, ...]:
+        return (np.log1p(-self.p),)
+
+    @property
+    def log_normalizer(self) -> Parameter:
+        return -np.log(self.p)
+
+    def base_measure(self, x: Variate) -> ArrayLike:
+        return 1.0
+
+    def sufficient_statistic(self, x: Variate) -> Tuple[ArrayLike, ...]:
+        return (x,)
+
+
+class NegativeBinomial(ExponentialFamily):
+    _constraints: Dict[str, Constraint] = {"r": positive, "p": zero_one}
+    _support: Constraint = non_negative_integer
+
+    def __init__(
+        self,
+        r: Parameter,
+        p: Parameter,
+        check_parameters: bool = True,
+        check_support: bool = True,
+    ):
+        batch_shape = broadcast_shapes(np.shape(r), np.shape(p))
+        rv_shape = ()
+
+        self.r, self.p = promote_shapes(r, p)
+
+        super().__init__(
+            batch_shape=batch_shape,
+            rv_shape=rv_shape,
+            check_parameters=check_parameters,
+            check_support=check_support,
+        )
+
+    @property
+    def mean(self) -> Parameter:
+        return self.r * self.p / (1.0 - self.p)
+
+    @property
+    def variance(self) -> Parameter:
+        return self.r * self.p / np.square(1.0 - self.p)
+
+    def _log_prob(self, x: Variate) -> ArrayLike:
+        return (
+            log_binomial_coefficient(self.r + x - 1, x)
+            + self.r * np.log1p(-self.p)
+            + x * np.log(self.p)
+        )
+
+    def _sample(self, sample_shape: Shape, random_state: RandomState) -> Variate:
+        return random_state.negative_binomial(
+            n=self.r, p=1.0 - self.p, size=sample_shape + self.batch_shape
+        )
+
+    @property
+    def natural_parameter(self) -> Tuple[Parameter, ...]:
+        return (np.log(self.p),)
+
+    @property
+    def log_normalizer(self) -> Parameter:
+        return -self.r * np.log1p(-self.p)
+
+    def base_measure(self, x: Variate) -> ArrayLike:
+        return binom(x + self.r - 1.0, x)
+
+    def sufficient_statistic(self, x: Variate) -> Tuple[ArrayLike, ...]:
+        return (x,)
+
+
+class Poisson(ExponentialFamily):
+    _constraints: Dict[str, Constraint] = {"rate": positive}
+    _support: Constraint = non_negative_integer
+
+    def __init__(
+        self, rate: Parameter, check_parameters: bool = True, check_support: bool = True
+    ):
+        batch_shape = np.shape(rate)
+        rv_shape = ()
+
+        self.rate = rate
+
+        super().__init__(
+            batch_shape=batch_shape,
+            rv_shape=rv_shape,
+            check_parameters=check_parameters,
+            check_support=check_support,
+        )
+
+    @property
+    def mean(self) -> Parameter:
+        return self.rate
+
+    @property
+    def variance(self) -> Parameter:
+        return self.rate
+
+    def _log_prob(self, x: Variate) -> ArrayLike:
+        return x * np.log(self.rate) - gammaln(x + 1) - self.rate
+
+    def _sample(self, sample_shape: Shape, random_state: RandomState) -> Variate:
+        return random_state.poisson(lam=self.rate, size=sample_shape + self.batch_shape)
+
+    @property
+    def natural_parameter(self) -> Tuple[Parameter, ...]:
+        return (np.log(self.rate),)
+
+    @property
+    def log_normalizer(self) -> Parameter:
+        return self.rate
+
+    def base_measure(self, x: Variate) -> ArrayLike:
+        return np.reciprocal(gamma(x + 1.0))
+
+    def sufficient_statistic(self, x: Variate) -> Tuple[ArrayLike, ...]:
+        return (x,)
