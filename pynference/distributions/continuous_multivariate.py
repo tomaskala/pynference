71e1f14afa357a8ce6a5effe17bcda3d28937fe5
Tomas Kala <tomik.kala@gmail.com>
2019-11-06 13:02:28 +0100

    Formatting changes

--- pynference/distributions/continuous_multivariate.py
+++ pynference/distributions/continuous_multivariate.py
@@ -5,19 +5,12 @@ from numpy.random import RandomState
 from scipy.special import gammaln
 
 from pynference.constants import ArrayLike, Parameter, Shape, Variate
-from pynference.distributions.constraints import (
-    Constraint,
-    positive,
-    simplex
-)
-
+from pynference.distributions.constraints import Constraint, positive, simplex
 from pynference.distributions.distribution import (
     Distribution,
     ExponentialFamily,
     TransformedDistribution,
 )
-
-from pynference.distributions.utils import broadcast_shapes, promote_shapes
 
 
 class Dirichlet(ExponentialFamily):
@@ -31,10 +24,12 @@ class Dirichlet(ExponentialFamily):
         check_support: bool = True,
     ):
         if np.ndim(concentration) < 1:
-            raise ValueError("The concentration parameter must be at least 1-dimensional.")
+            raise ValueError(
+                "The concentration parameter must be at least 1-dimensional."
+            )
 
-        batch_shape = concentration.shape[:-1]
-        rv_shape = concentration.shape[-1:]
+        batch_shape = np.shape(concentration)[:-1]
+        rv_shape = np.shape(concentration)[-1:]
 
         self.concentration = concentration
         self.concentration_sum = np.sum(concentration, axis=-1, keepdims=True)
@@ -43,7 +38,7 @@ class Dirichlet(ExponentialFamily):
             batch_shape=batch_shape,
             rv_shape=rv_shape,
             check_parameters=check_parameters,
-            check_support=check_support
+            check_support=check_support,
         )
 
     @property
@@ -56,26 +51,32 @@ class Dirichlet(ExponentialFamily):
         return mean * (1.0 - mean) / (self.concentration_sum + 1.0)
 
     def _log_prob(self, x: Variate) -> ArrayLike:
-        normalizer = np.sum(gammaln(self.concentration), axis=-1) - gammaln(np.sum(self.concentration, axis=-1))
+        normalizer = np.sum(gammaln(self.concentration), axis=-1) - gammaln(
+            np.sum(self.concentration, axis=-1)
+        )
         return np.sum((self.concentration - 1.0) * np.log(x), axis=-1) - normalizer
 
     def _sample(self, sample_shape: Shape, random_state: RandomState) -> Variate:
-        epsilon = random_state.standard_gamma(self.concentration, sample_shape + self.batch_shape + self.rv_shape)
+        epsilon = random_state.standard_gamma(
+            self.concentration, sample_shape + self.batch_shape + self.rv_shape
+        )
         return epsilon / np.sum(epsilon, axis=-1, keepdims=True)
 
     @property
     def natural_parameter(self) -> Tuple[Parameter, ...]:
-        return self.concentration,
+        return (self.concentration,)
 
     @property
     def log_normalizer(self) -> Parameter:
-        return np.sum(gammaln(self.concentration), axis=-1) - gammaln(np.sum(self.concentration, axis=-1))
+        return np.sum(gammaln(self.concentration), axis=-1) - gammaln(
+            np.sum(self.concentration, axis=-1)
+        )
 
     def base_measure(self, x: Variate) -> ArrayLike:
         return 1.0 / np.product(x, axis=-1)
 
     def sufficient_statistic(self, x: Variate) -> Tuple[ArrayLike, ...]:
-        return np.log(x),
+        return (np.log(x),)
 
 
 class InverseWishart(TransformedDistribution):
@@ -87,9 +88,7 @@ class MultivariateNormal(ExponentialFamily):
     _support: Constraint = None
 
     def __init__(
-        self,
-        check_parameters: bool = True,
-        check_support: bool = True,
+        self, check_parameters: bool = True, check_support: bool = True,
     ):
         pass
 
@@ -127,9 +126,7 @@ class MultivariateT(Distribution):
     _support: Constraint = None
 
     def __init__(
-        self,
-        check_parameters: bool = True,
-        check_support: bool = True,
+        self, check_parameters: bool = True, check_support: bool = True,
     ):
         pass
 
@@ -153,9 +150,7 @@ class Wishart(ExponentialFamily):
     _support: Constraint = None
 
     def __init__(
-        self,
-        check_parameters: bool = True,
-        check_support: bool = True,
+        self, check_parameters: bool = True, check_support: bool = True,
     ):
         pass
 
